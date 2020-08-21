ea3d65b90058d3cf0013d6866496e6ae25bd2ca3
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-08-21 01:14:32 -0300

    Chat rodando com banco de dados

--- public/script.js
+++ public/script.js
@@ -1,53 +1,66 @@
 const caixaDeTexto = document.getElementById('caixa-de-texto');
-
 const socket = io('http://localhost:3005/');
+
 let nomeUsuario = prompt('Qual seu nome?');
 
 if (nomeUsuario === null || nomeUsuario === '') {
   nomeUsuario = `Usuário #${[Math.floor(Math.random() * 50)]}`;
 }
 
-socket.emit('historico', { nomeUsuario });
+const pegaHistorico = async () => {
+  const { data } = await axios.get('http://localhost:3000/messages');
+  return data;
+};
+
+const criaUsuario = async (user) => axios
+  .post('http://localhost:3000/name', { name: user });
+
+const salvaMensagem = async (messageInfo) => axios
+  .post('http://localhost:3000/messages', messageInfo);
 
 const enviarMensagem = () => {
   const mensagemEnviada = caixaDeTexto.value;
   if (mensagemEnviada.length > 0) {
+    salvaMensagem({ nomeUsuario, mensagemEnviada, data: new Date() });
     socket.emit('mensagemChat', { nomeUsuario, mensagemEnviada });
     caixaDeTexto.value = '';
   }
 };
 
-socket.on('historico', (msg) => {
+const colocaMensagemNoDOM = (msg, classe) => {
   const ul = document.getElementById('mensagens');
   const li = document.createElement('li');
-  li.classList.add('historico');
-  li.appendChild(document.createTextNode(msg));
-  ul.appendChild(li);
-});
-
-socket.on('novoUsuario', (msg) => {
-  const ul = document.getElementById('mensagens');
-  const li = document.createElement('li');
-  li.classList.add('novo_usuario');
-  li.appendChild(document.createTextNode(msg));
-  ul.appendChild(li);
-});
-
-socket.on('mensagemChat', (msg) => {
-  const ul = document.getElementById('mensagens');
-  const li = document.createElement('li');
+  li.classList.add(classe);
   li.appendChild(document.createTextNode(msg));
   ul.appendChild(li);
   ul.scrollTop = ul.scrollHeight;
-});
+};
 
 caixaDeTexto.addEventListener('keypress', (e) => {
-  console.log('fora');
   const key = e.which || e.keyCode;
   if (key === 13) {
-    console.log('dentro');
     enviarMensagem('http://localhost:3005');
   }
+});
+
+const carregarChat = async () => {
+  await criaUsuario(nomeUsuario);
+  const mensagensDB = await pegaHistorico();
+  await socket.emit('historico', { nomeUsuario, mensagensDB });
+};
+
+carregarChat();
+
+socket.on('historico', (msg) => {
+  colocaMensagemNoDOM(msg, 'historico');
+});
+
+socket.on('novoUsuario', (msg) => {
+  colocaMensagemNoDOM(msg, 'novo_usuario');
+});
+
+socket.on('mensagemChat', (msg) => {
+  colocaMensagemNoDOM(msg, 'mensagens');
 });
 
 document.getElementById('envia-msg').addEventListener('click', enviarMensagem);
--- server.js
+++ server.js
@@ -1,33 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const socketServer = require('http').createServer();
 const io = require('socket.io')(socketServer);
 const { join } = require('path');
+const messagesController = require('./controllers/messagesController');
 
 const app = express();
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', express.static(join(__dirname, 'public')));
 
+app.get('/messages', messagesController.getAllMessages);
+app.post('/messages', messagesController.createMessage);
+app.post('/name', messagesController.createName);
+
 io.on('connection', (socket) => {
-  socket.on('historico', ({ nomeUsuario }) => {
-    const array = ['hist00', 'hist01', 'hist99'];
-    array.forEach((elem) => socket.emit('historico', elem));
-
-    const horarioNumSegundos = new Date();
-    const horarioLocale = horarioNumSegundos.toLocaleString();
-
-    io.emit('novoUsuario', `${horarioLocale} - Boas vindas ${nomeUsuario}`);
+  socket.on('historico', (info) => {
+    const { mensagensDB } = info;
+    mensagensDB.forEach(({ name, messages }) => socket
+      .emit(
+        'historico',
+        `${new Date(messages.postedAt).toLocaleString()} - ${name}: ${messages.content}`,
+      ));
+    const horarioLocale = new Date().toLocaleString();
+    io.emit('novoUsuario', `${horarioLocale} - Bem vindo(a) ${info.nomeUsuario}`);
   });
 
   socket.on('mensagemChat', ({ nomeUsuario, mensagemEnviada }) => {
-    const horarioNumSegundos = new Date();
-    const horarioLocale = horarioNumSegundos.toLocaleString();
-
+    const horarioLocale = new Date().toLocaleString();
     io.emit('mensagemChat', `${horarioLocale} - ${nomeUsuario}: ${mensagemEnviada}`);
   });
 });
