99fcb8ca934f71162c37713e479ca61d3849f769
richardwellerson <richardwellerson@gmail.com>
2020-09-01 08:57:51 -0300

    Server not listening

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const app = require('express')();
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, { origins: '*:*' });
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const SOCKET = process.env.SOCKET;
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(cors({ allowedHeaders: '*' }));
+app.use(bodyParser.json());
 
 io.on('connection', (socket) => {
   console.log('conectado');
@@ -18,3 +20,4 @@ io.on('connection', (socket) => {
 });
 
 app.listen(PORT, () => console.log(`Listen on ${PORT}`));
+io.listen(SOCKET);
