6a6369fe1d2dd3f1654bf354d55aa34d34f0cd7b
Richard Wellerson <richardwellerson@gmail.com>
2020-10-14 19:58:12 -0300

    Socket funcionando

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
-const app = require('express')();
-const http = require('http').createServer(app)
-const io = require('socket.io')(http, { origins: '*:*' });
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const socketServer = require('http').createServer();
+const io = require('socket.io')(socketServer);
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -9,15 +14,31 @@ const SOCKET = process.env.SOCKET;
 const PORT = process.env.PORT;
 
 app.use(cors({ allowedHeaders: '*' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 io.on('connection', (socket) => {
-  console.log('conectado');
+
+  console.log(`${socket.id} conectado`);
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('serverResponse', {
+      message,
+      clientId: socket.id,
+    });
+  });
 
   socket.on('disconnect', () => {
     console.log('desconectado.')
-  })
+  });
 });
 
-app.listen(PORT, () => console.log(`Listen on ${PORT}`));
-io.listen(SOCKET);
+
+app.listen(PORT);
+console.log(`Listen on ${PORT}`)
+
+socketServer.listen(SOCKET);
+console.log(`Socket on ${SOCKET}`);
