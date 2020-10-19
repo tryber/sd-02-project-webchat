d437dd0cc2cd302d523980ca44957b90d2c39804
Richard Wellerson <richardwellerson@gmail.com>
2020-10-18 21:44:15 -0300

    Create Seed

---
+++ back/seed.js
@@ -0,0 +1,16 @@
+/*
+
+use webchat;
+
+db.createCollection("messages");
+
+db.messages.insertMany([
+  {"nickname": "twain", "message":"Hi!" },
+  {"nickname": "cezar", "message":"Hi!" },
+  {"nickname": "joseph", "message":"Hi!" },
+  {"nickname": "1917warrior", "message":"Hi!" },
+  {"nickname": "roiroz", "message":"Hi!" },
+  {"nickname": "aramuniShooter", "message":"Hi!" },
+]);
+
+*/
